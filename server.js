f05ac48f446c9a1aa47e1e42304e79011b5353ef
Gounaya <o.gounaya@gmail.com>
2018-06-05 23:43:16 +0200

    not finished yet

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
+let session = require('express-session')
+
 // Moteur de template
 app.set('view engine', 'ejs')
 
@@ -8,15 +10,23 @@ app.set('view engine', 'ejs')
 app.use('/assets',express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(session({
+    secret: 'azeaze',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{secure: true}
+}))
 
 // Routes
 app.get('/', (request, response) => {
+    console.log(request.session.error)
     response.render('pages/index', {test: 'Salut'})
 })
 
 app.post('/', (request, response) => {
     if (request.body.message === undefined || request.body.message === ''){
-        response.render('pages/index', {error: "Vous n'avez pas entré de message :( "})
+        request.session.error = "Il y a une erreur"
+        response.redirect('/')
 }
 })
 app.listen(8080)
