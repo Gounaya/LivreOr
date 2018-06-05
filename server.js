58230361a0d5223c9b111092ce0f11b213f31bd1
Gounaya <o.gounaya@gmail.com>
2018-06-05 23:21:29 +0200

    solve probleme header.ejs not js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 let express = require('express')
 let app = express()
-
+let bodyParser = require('body-parser')
+// Moteur de template
 app.set('view engine', 'ejs')
 
+// Middleware
 app.use('/assets',express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
+// Routes
 app.get('/', (request, response) => {
     response.render('pages/index', {test: 'Salut'})
 })
 
+app.post('/', (request, response) => {
+    if (request.body.message === undefined || request.body.message === ''){
+        response.render('pages/index', {error: "Vous n'avez pas entré de message :( "})
+}
+})
 app.listen(8080)
 
